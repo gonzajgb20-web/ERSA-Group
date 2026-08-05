9ce93cba0c4114a2d92836f3e846e0880036aba5
gonzajgb20-web <gonzajgb20@gmail.com>
2026-08-05 14:54:19 -0300

    Fix react-router-dom import typo in Header.tsx

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useLocation } from 'react.router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Download, Settings, ArrowLeft, LogOut } from 'lucide-react';
 import { useAuthStore } from '../store/authStore';
 
